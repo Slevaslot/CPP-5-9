83e065ed04f9fc4d15cc6e323baac9a2cc082360
slevaslo <souleimane.lvt@gmail.com>
2024-04-04 19:33:37 +0200

    cpp09

---
+++ cpp08/ex02/ MutantStack.hpp
@@ -0,0 +1,12 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+
+class MutantStack : public std::stack<int>
+{
+	
+};
+
+#endif
---
+++ cpp09/ex00/main.cpp
@@ -0,0 +1,16 @@
+#include "BitcoinExchange.hpp"
+
+int main (int ac, char **av)
+{
+	std::fstream data;
+	data.open("data.csv", std::fstream::in);
+	if (data.is_open())
+	{
+
+	}
+	else {
+    std::cerr << "Error opening file!" << std::endl;
+  }
+}
+'
+'
