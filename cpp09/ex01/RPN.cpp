519e23b7a96c9d3eeba0c070a15f33ee2f5ec629
slevaslo <souleimane.lvt@gmail.com>
2024-04-21 17:21:12 +0200

    ex01 cpp01 clean

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -55,16 +55,3 @@ int evaluateRPN(const std::string& expression) {
     else
         throw std::invalid_argument("not enough operator to operate.");
 }
-
-int main(int ac, char **av) {
-    if (ac < 2)
-        return (std::cout << "Error: invalid format, ex: ./RPN value value ..." << std::endl, -1);
-    try {
-            int result = evaluateRPN(av[1]);
-            std::cout << "Le résultat de l'expression RPN est : " << result << std::endl;
-    }
-    catch(std::exception &e)
-    {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
-}
---
+++ cpp09/ex01/RPN.hpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <stack>
+#include <sstream>
+#include <cstdlib>
+
+bool isInteger(std::string token);
+int evaluateRPN(const std::string& expression);
